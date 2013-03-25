d22e07f21ca3c51a6983c9aa04468e58ef4bd46e
Arkadiy Kraportov <arkadiyk@gmail.com>
2013-03-25 18:32:17 +0900

    fixed api mock up to match google api

--- js/fake_google_drive_api/google_fake_api.js
+++ js/fake_google_drive_api/google_fake_api.js
@@ -9,25 +9,28 @@ window.gapi = {
         }
     },
 	client: {
+        load: function(api_name, api_version, load_callback) {
+            setTimeout(load_callback, 1000)
+        },
 		drive: {
 			files: {
 				list: function(params) {
                     var folder_id = params.q.match(/'(.*)' in parents/)[1];
                     var result = GoogDriveFixtures.file_list[folder_id];
-					return function(callback) {
+					return {execute: function(callback) {
                         console.log("calling list with ", params, folder_id, result);
-						setTimeout(function(){ callback(result) }, 1200);
-					}
+						setTimeout(function(){ callback(result) }, 1000);
+					}}
 				}
 			},
             parents: {
                 list: function(params) {
                     var folder_id = params.fileId;
                     var result = GoogDriveFixtures.parent_list[folder_id];
-                    return function(callback) {
+                    return {execute: function(callback) {
                         console.log("calling parents with ", params, folder_id, result);
-                        setTimeout(function(){ callback(result) }, 1200);
-                    }
+                        setTimeout(function(){ callback(result) }, 1000);
+                    }}
                 }
             }
         }
